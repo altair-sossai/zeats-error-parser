8789f85b80d5a9899144b69036934af2077ace71
Altair Sossai <altairsossai@gmail.com>
2021-02-05 17:10:36 -0300

    Inicio do projeto

---
+++ Zeats.Error.Parser/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FluentValidation;
+using Zeats.Error.Extensions;
+using Zeats.Error.FluentValidation.Extensions;
+
+namespace Zeats.Error.Parser.Extensions
+{
+    public static class ExceptionExtensions
+    {
+        public static ErrorInfo ErrorInfo(this Exception exception)
+        {
+            if (exception is ValidationException validationException)
+                return validationException.ToErrorInfo();
+
+            return exception.ToErrorInfo();
+        }
+    }
+}
