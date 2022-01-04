d3a3c992a66d7864c27ed9330a466199f62eb663
Altair Sossai <altair_sossai@outlook.com>
2022-01-04 09:48:45 -0800

    upgrade to net6.0

--- Zeats.Error.Parser/Extensions/ExceptionExtensions.cs
+++ Zeats.Error.Parser/Extensions/ExceptionExtensions.cs
@@ -3,16 +3,15 @@ using FluentValidation;
 using Zeats.Error.Extensions;
 using Zeats.Error.FluentValidation.Extensions;
 
-namespace Zeats.Error.Parser.Extensions
-{
-    public static class ExceptionExtensions
-    {
-        public static ErrorInfo ErrorInfo(this Exception exception)
-        {
-            if (exception is ValidationException validationException)
-                return validationException.ToErrorInfo();
+namespace Zeats.Error.Parser.Extensions;
 
-            return exception.ToErrorInfo();
-        }
+public static class ExceptionExtensions
+{
+    public static ErrorInfo ErrorInfo(this Exception exception)
+    {
+        if (exception is ValidationException validationException)
+            return validationException.ToErrorInfo();
+
+        return exception.ToErrorInfo();
     }
 }
